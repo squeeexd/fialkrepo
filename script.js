afe4d009489f204c015c8afe06ad4c829faecffa
floryw <pav.obuhov@gmail.com>
2025-09-16 15:36:45 +0300

    initial message

---
+++ script.js
@@ -0,0 +1,99 @@
+// // const cats = 23+23;
+// // console.log(cats);
+// // cats = 47;
+// // console.log(cats);
+// // cats += 10;
+// // // console.log(cats);
+// // function fun1(a, b, c, d) {
+// //     console.log('Привет')
+// //     console.log(a+b+c+d);
+// // }
+// // let cats = 5
+// // fun1(5, 6, 7, 8)
+
+// // let a = 7
+// // let b = 6
+// // let c = 9
+
+// // function plus(number1, number2) { //функция с плюсиком между чиселками
+// //     console.log(number1+number2); 
+// // }
+// // plus(a, b)                        //вызываем эту функцию
+
+// // function vozvrashenie(number1, number2) { //функция с минусиком между чиселками
+// //     return number1-number2                //вывод с помощью слова return
+// // }
+// // console.log(vozvrashenie(a, b))           //вызов этой функции учитывая ее return
+
+// // function plus1(number1, number2, number3) { //функция с двумя плюсиками между 3 чиселками
+// //     console.log(number1+number2+number3); 
+// // }
+// // plus1(a, b, c)                              
+
+// // function vozvrashenie1(number2, number3) {  //функция с минусиком между чиселками
+// //     return number3-number2                  //вывод с помощью слова return
+// // }
+// // console.log(vozvrashenie1(b, c))            //вызов этой функции учитывая ее return
+
+
+// function sayHi1 (){
+//     alert("Привееет")
+// }
+
+// function sayHi2 (){
+//     alert("Максим крутой перец")
+// }
+
+// function sayHi3 (){
+//     alert("Никита тоже крутой перец")
+// }
+
+// const sq1 = document.getElementById("sq1") 
+// sq1.addEventListener("click", sayHi1)
+
+// const sq2 = document.getElementById("sq2") 
+// sq2.addEventListener("click", sayHi2)
+
+// const sq3 = document.getElementById("sq3") 
+// sq3.addEventListener("click", sayHi3)
+
+// const inp = document.getElementById('inp')
+// console.log(inp.value);
+// inp.value=''
+const inp = document.getElementById('inp')
+const inp2 = document.getElementById('inp2')
+const btn = document.getElementById('btn')
+const result = document.getElementById('result')
+
+check = btn.addEventListener('click', logVal)
+
+
+function logVal() {
+    if (!isNaN(inp.value) && (!isNaN(inp2.value))) {
+        result.classList.remove('error')
+        result.textContent = Number(inp.value)*Number(inp2.value)
+    } 
+    else {
+        result.classList.add('error')
+        result.textContent = 'Вы ввели не число!!'
+    }
+    
+    inp.value=('')
+    inp2.value=('')
+}
+
+// let cond = 5
+// switch (cond) {
+//     case 1: 
+//     console.log(1);
+//     break;
+//     case 2: 
+//     console.log(2);
+//     break;
+
+//     default:
+//         console.log('Ни 1 ни 2')
+//         break;
+// }
+
+
